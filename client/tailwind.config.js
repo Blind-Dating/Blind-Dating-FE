4594c448ff7c1f68e611cf8b9a9bf50e328fa5a1
ted-jv <wkejrdldls@gmail.com>
2023-08-01 19:51:24 +0900

    [Package] tailwind css 클래스화 적용 & tailwind root css 적용

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      red: '#E94057',
+      lightBlue: 'hsl(233, 100%, 69%)',
+      darkGrayishBlue: 'hsl(210, 10%, 33%)',
+      grayishBlue: 'hsl(201, 11%, 66%)',
+    },
+    fontFamily: {
+      sans: ['sans-serif'],
+      serif: ['serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        none: '0',
+        sm: '.125rem',
+        DEFAULT: '.25rem',
+        lg: '.5rem',
+        full: '9999px',
+      },
+    },
+  },
+  plugins: [],
 };
