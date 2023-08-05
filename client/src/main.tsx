b6ca608cd1a1cda7f2828d0e549c9df6683f39aa
ted-jv <wkejrdldls@gmail.com>
2023-08-05 19:16:44 +0900

    [Feat] add web font 적용

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from 'routes/router';
+import './index.css';
+import '@fontsource/noto-sans';
+import '@fontsource/lobster';
+import '@fontsource-variable/lora';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
