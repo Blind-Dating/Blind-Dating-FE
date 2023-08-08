6394fe790e0913355f57a411acbc6c207097e4d9
ted-jv <wkejrdldls@gmail.com>
2023-08-08 14:19:50 +0900

    [Style] add NoHeaderFooterLayout 스타일 적용

--- client/src/components/layout/NoHeaderFooterLayout.tsx
+++ client/src/components/layout/NoHeaderFooterLayout.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function NoHeaderFooterLayout({ children }: { children: React.ReactNode }) {
-  return <div className="container max-w-[812px] mx-auto pt-16">{children}</div>;
+  return (
+    <div className="container flex flex-col justify-center items-center mx-auto w-[375px] h-[812px]  pt-32">
+      {children}
+    </div>
+  );
 }
 
 export default NoHeaderFooterLayout;
