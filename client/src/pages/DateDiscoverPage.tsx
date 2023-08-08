00b5a619c75cb7a4bd41e6a5d9fa56de98c763a2
ted-jv <wkejrdldls@gmail.com>
2023-08-08 17:23:38 +0900

    [Feat] add DataDiscoverPage 생성 & router 등록

---
+++ client/src/pages/DateDiscoverPage.tsx
@@ -0,0 +1,8 @@
+import Layout from 'components/layout/Layout';
+import React from 'react';
+
+function DateDiscoverPage() {
+  return <Layout>DateDiscover</Layout>;
+}
+
+export default DateDiscoverPage;
