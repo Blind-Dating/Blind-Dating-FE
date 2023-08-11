ba58d8ac2e501c9e6b15ef69f47783fa3f3ec65a
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-11 16:50:18 +0900

    [Style] add navbar markup

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { ReactComponent as Setting } from '/public/icons/setting-config.svg';
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+type Props = {
+  title?: string;
+};
+
+const Navbar = (props: Props) => {
+  const { title } = props;
+  return (
+    <header className="my-10 mx-8 flex justify-between items-center">
+      <h1 className="text-2xl font-bold font-Lora">{title}</h1>
+      {title === 'Messages' && (
+        <button
+          type="button"
+          className="w-12 h-12 p-3 text-redAmaranth border border-whiteSmoke rounded-xl"
+        >
+          <Setting />
+        </button>
+      )}
+    </header>
+  );
+};
 
 export default Navbar;
