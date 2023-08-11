69e2b2f9abfe58b5c8c335dbbc22c050b23c3293
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-11 17:48:48 +0900

    [Style] add footer markup

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { ReactComponent as Cards } from '/public/icons/cards.svg';
+import { ReactComponent as Likes } from '/public/icons/likes.svg';
+import { ReactComponent as Messages } from '/public/icons/messages.svg';
+import { ReactComponent as User } from '/public/icons/user.svg';
+
+const FooterBtn = ({ icon }: { icon: React.ReactNode }) => {
+  return (
+    <button type="button" className="w-16 h-12 flex justify-center items-center">
+      {icon}
+    </button>
+  );
+};
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex justify-around bg-whiteLilac pb-8 mt-8">
+      <FooterBtn icon={<Cards />} />
+      <FooterBtn icon={<Likes />} />
+      <FooterBtn icon={<Messages />} />
+      <FooterBtn icon={<User />} />
+    </footer>
+  );
 }
 
 export default Footer;
