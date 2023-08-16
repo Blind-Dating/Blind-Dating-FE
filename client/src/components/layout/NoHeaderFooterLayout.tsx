fc514dadf3284bcf13d78e688a20290f306eead3
ted-jv <wkejrdldls@gmail.com>
2023-08-16 19:36:48 +0900

    [Style] add margin-auto css

--- client/src/components/layout/NoHeaderFooterLayout.tsx
+++ client/src/components/layout/NoHeaderFooterLayout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NoHeaderFooterLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="container flex flex-col justify-center items-center  w-[375px] h-[812px]">
+      <div className="container flex flex-col justify-center items-center m-auto w-[375px] h-[812px]">
         {children}
       </div>
     </div>
