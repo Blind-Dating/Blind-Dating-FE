d1bdd1b89bbc50f34d835a547d08e899c6554051
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-17 22:05:50 +0900

    [Style] add chat page textarea style

--- client/src/components/chat/ChatForm.tsx
+++ client/src/components/chat/ChatForm.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const ChatForm = () => {
   return (
-    <div>
-      <h1>form</h1>
-    </div>
+    <section className="w-full max-h-[15%] px-10 py-8">
+      <textarea
+        className="w-full p-2 text-sm border rounded resize-none border-whiteLilac"
+        placeholder="your message"
+      />
+    </section>
   );
 };
 
