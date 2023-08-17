532bc834ff609586269439de18ef037860de83d8
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-17 17:17:58 +0900

    [Feat] add Chat page router

---
+++ client/src/pages/ChatPage.tsx
@@ -0,0 +1,13 @@
+import NoHeaderFooterLayout from 'components/layout/NoHeaderFooterLayout';
+
+import React from 'react';
+
+const ChatPage = () => {
+  return (
+    <NoHeaderFooterLayout>
+      <div>Chat</div>
+    </NoHeaderFooterLayout>
+  );
+};
+
+export default ChatPage;
