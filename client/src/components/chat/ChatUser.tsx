a5157a6c1353ff5c8ba6b3c6c2f38028e112953d
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-17 21:02:23 +0900

    [Style] add chat page 더미 데이터 추가

--- client/src/components/chat/ChatUser.tsx
+++ client/src/components/chat/ChatUser.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ReactComponent as More } from '/public/icons/more.svg';
 
-const ChatUser = () => {
+type Props = { id: number; name: string; image: string; status: string };
+
+const ChatUser = (props: Props) => {
+  const { name, image, status } = props;
   return (
     <section className="flex items-center w-full gap-2 px-10 py-8">
-      <div className="w-12 h-12 rounded-full bg-labelColor">{''}</div>
+      <div className="w-12 h-12 rounded-full bg-labelColor">{image}</div>
       <div className="flex flex-col flex-1">
-        <h2 className="text-xl font-bold font-Lora">{'Grace'}</h2>
-        <b className="mx-1 text-xs text-labelColor">{'Online'}</b>
+        <h2 className="text-xl font-bold font-Lora">{name}</h2>
+        <b className="mx-1 text-xs text-labelColor">{status}</b>
       </div>
       <button
         type="button"
--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -5,11 +5,50 @@ import NoHeaderFooterLayout from 'components/layout/NoHeaderFooterLayout';
 
 import React from 'react';
 
+const chatDummy = {
+  id: 0,
+  user: { id: 0, name: 'Grace', image: '', status: 'Online' },
+  messages: [
+    {
+      messageId: 0,
+      content:
+        "I got my peaches out in Georgia (oh, yeah, sh*t) I get my weed from California (that's that sh*t)",
+      senderId: 0,
+      created: '2023. 8. 16.',
+    },
+    {
+      messageId: 1,
+      content:
+        "I took my chick up to the North, yeah (badass b*tch)      I get my light right from the source, yeah (yeah, that's it)",
+      senderId: 1,
+      created: '2023. 8. 16.',
+    },
+    {
+      messageId: 2,
+      content: "And I see you (oh), the way I breathe you in (in), it's the texture of your skin",
+      senderId: 0,
+      created: new Date().toLocaleDateString(),
+    },
+    {
+      messageId: 3,
+      content: 'I wanna wrap my arms around you, baby, never let you go, oh',
+      senderId: 1,
+      created: new Date().toLocaleDateString(),
+    },
+    {
+      messageId: 4,
+      content: "And I say, oh, there's nothing like your touch",
+      senderId: 0,
+      created: new Date().toLocaleDateString(),
+    },
+  ],
+};
+
 const ChatPage = () => {
   return (
     <NoHeaderFooterLayout>
-      <ChatUser />
-      <ChatMessages />
+      <ChatUser {...chatDummy.user} />
+      <ChatMessages key={chatDummy.id} messages={chatDummy.messages} />
       <ChatForm />
     </NoHeaderFooterLayout>
   );
