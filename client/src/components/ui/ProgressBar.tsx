d5b0697e0a2ad7c058807f1e6d3537a103a44e61
ted-jv <wkejrdldls@gmail.com>
2023-08-18 22:10:57 +0900

    [Fix] modify ProgressBar 동적 width css 해결

--- client/src/components/ui/ProgressBar.tsx
+++ client/src/components/ui/ProgressBar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 type Props = {
-  width: number;
+  width: string;
 };
 
 export const ProgressBar = ({ width }: Props) => {
+  const widthConfig: Record<string, string> = {
+    '3/4': 'w-3/4',
+  };
   return (
     <div className="w-full h-2 mb-9 bg-grayLignt">
-      <div className={`"${width} h-full duration-300 ease-in-out bg-redAmaranth" `}></div>
+      <div className={`${widthConfig[width]} h-full duration-300 ease-in-out bg-redAmaranth`}></div>
     </div>
   );
 };
