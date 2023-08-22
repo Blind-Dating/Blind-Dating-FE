2163772c842d5626b06d8f418d9b76a167e2ea86
ted-jv <wkejrdldls@gmail.com>
2023-08-22 18:31:19 +0900

    [Feat] add personality-form 기능

--- client/src/components/sign-up/personality-test/PersonalityTestForm.tsx
+++ client/src/components/sign-up/personality-test/PersonalityTestForm.tsx
@@ -1,8 +1,8 @@
-import { ProgressBar } from 'components/ui/ProgressBar';
-import React from 'react';
+import React, { useState } from 'react';
 import { QuestionField } from './QuestionField';
 import { QUESTIONS } from 'assets/config';
 import { SignUpAllValues } from 'pages/SignUpPage';
+import { Header } from 'components/layout/Header';
 
 type Props = {
   onNext: () => void;
@@ -10,25 +10,39 @@ type Props = {
 };
 
 const PersonalityTestForm = ({ onNext, setSignUpAllValues }: Props) => {
+  const [collectAnswers, setCollectAnswers] = useState<(boolean | null)[]>(Array(8).fill(null));
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSignUpAllValues((prev) => ({ ...prev, questions: collectAnswers }));
+    onNext();
+  };
+
   return (
     <div className="w-full h-full">
-      <header className="w-full">
-        <ProgressBar width={3 / 4} />
-        <h1 className="text-3xl font-bold pl-11 font-Lora">Profile details</h1>
-      </header>
+      <Header progressWidth="3/4" title="Profile details" />
 
       <p className="w-full pr-16 mt-3 font-medium text-s h-9 pl-11 text-grayIsh">
         질문에 대한 답변은 상대방에게 내가 어떤 사람인지 알리는데 많은 도움이 됩니다.
       </p>
 
       <main className="mt-10 mb-[38px] px-7">
-        <form className="flex flex-col items-center justify-center w-full ">
-          <main className="space-y-8 h-[480px] overflow-y-auto">
+        <form className="flex flex-col items-center justify-center w-full " onSubmit={handleSubmit}>
+          <main className="space-y-8 h-[480px] overflow-y-auto no-scrollbar">
             {QUESTIONS.map((question, index) => {
-              return <QuestionField key={index} question={question} index={index + 1} />;
+              return (
+                <QuestionField
+                  key={index}
+                  question={question}
+                  index={index + 1}
+                  setCollectAnswers={setCollectAnswers}
+                />
+              );
             })}
           </main>
-          <button className="mt-10 btn-red"> Continue </button>
+          <button className="mt-16 btn-red" disabled={collectAnswers.includes(null)}>
+            Continue
+          </button>
         </form>
       </main>
     </div>
