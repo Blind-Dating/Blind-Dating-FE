7c61a4c27d62a5c3e723e18e1fc36659fea62757
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-22 12:24:48 +0900

    [Refactor] useEffect 제거

--- client/src/components/chats/ChatList.tsx
+++ client/src/components/chats/ChatList.tsx
@@ -1,43 +1,36 @@
-import { useEffect, useState } from 'react';
 import ChatItem from './ChatItem';
 import { useGetChatRooms } from 'hooks/api/useGetChatRooms';
 
 function ChatList() {
-  const [chatList, setChatList] = useState([]);
   const { isLoading, data } = useGetChatRooms();
-  useEffect(() => {
-    if (!isLoading) {
-      setChatList(data.data.content);
-    }
-  }, [isLoading]);
+
+  if (isLoading) {
+    return (
+      <ul className="flex flex-col flex-1 w-full gap-2 px-8 overflow-auto">
+        <div
+          className="flex-1 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </ul>
+    );
+  }
 
   return (
-    <>
-      <ul className="flex flex-col w-full gap-2 px-8 overflow-auto h-144">
-        {isLoading ? (
-          <div
-            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-              Loading...
-            </span>
-          </div>
-        ) : (
-          <>
-            {chatList.length ? (
-              <>
-                {chatList.map((chat, idx) => (
-                  <ChatItem key={idx} {...chat} />
-                ))}
-              </>
-            ) : (
-              <li>No data</li>
-            )}
-          </>
-        )}
-      </ul>
-    </>
+    <ul className="flex flex-col w-full gap-2 px-8 overflow-auto flex-3">
+      {data.data && data.data.content.length ? (
+        <>
+          {data.data.content.map((chat) => (
+            <ChatItem key={chat.roomId} {...chat} />
+          ))}
+        </>
+      ) : (
+        <li>No data</li>
+      )}
+    </ul>
   );
 }
 
