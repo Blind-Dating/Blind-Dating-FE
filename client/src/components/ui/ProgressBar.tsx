26ee16bed1f7152ea50c24670985f953beba0b12
ted-jv <wkejrdldls@gmail.com>
2023-08-22 18:38:33 +0900

    [Chore] modify ProgressBar props 이름 변경으로 인한 변경

--- client/src/components/ui/ProgressBar.tsx
+++ client/src/components/ui/ProgressBar.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 type Props = {
-  width: string;
+  progressWidth: string;
 };
 
-export const ProgressBar = ({ width }: Props) => {
+export const ProgressBar = ({ progressWidth }: Props) => {
   const widthConfig: Record<string, string> = {
     '3/4': 'w-3/4',
   };
+
   return (
     <div className="w-full h-2 mb-9 bg-grayLignt">
-      <div className={`${widthConfig[width]} h-full duration-300 ease-in-out bg-redAmaranth`}></div>
+      <div
+        className={`${widthConfig[progressWidth]} h-full duration-300 ease-in-out bg-redAmaranth`}
+      ></div>
     </div>
   );
 };
