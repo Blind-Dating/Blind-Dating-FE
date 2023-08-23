40a9b1a42dab44fc77eaf2ce1505cf5bf14bda26
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-23 12:21:37 +0900

    [Style] modify layout style

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -4,8 +4,8 @@ import Footer from './Footer';
 
 function Layout({ title, children }: { title: string; children: React.ReactNode }) {
   return (
-    <div className="flex items-center justify-center ">
-      <div className="container w-[375px] h-[812px] flex flex-col">
+    <div className="flex items-center justify-center h-screen ">
+      <div className="container w-[375px] h-[812px] flex flex-col m-auto">
         <Navbar title={title} />
         <main className="flex-auto">{children}</main>
         <Footer />
