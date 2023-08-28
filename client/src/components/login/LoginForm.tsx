41abf7913cfcc2f67bebe9662a13271133e9eee8
ted-jv <wkejrdldls@gmail.com>
2023-08-28 17:33:35 +0900

    [Style] modify 로그인 버튼 클릭시에도 위에 회원가입 버튼 보이게 변경

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { usePostLogin } from 'hooks/api/usePostLogin';
 import React, { FormEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export type LoginFormValues = {
   userId: string;
@@ -15,43 +16,48 @@ export const LoginForm = () => {
     postLoginFn(loginInfo);
   };
   return (
-    <div className="mt-36">
-      <form className="flex flex-col items-center justify-center gap-3" onSubmit={handleSubmit}>
-        <input
-          className="border border-whiteSmoke rounded-2xl w-[295px] h-14 text-center font-NotoSans placeholder:italic focus:border-redAmaranth focus:outline-none"
-          type="text"
-          placeholder="Id"
-          value={loginInfo.userId}
-          autoFocus
-          required
-          onChange={(event) =>
-            setLoginInfo((prev) => ({
-              ...prev,
-              userId: event.target.value,
-            }))
-          }
-        />
-        <input
-          className="border border-whiteSmoke rounded-2xl w-[295px] h-14 text-center font-NotoSans  placeholder:italic focus:border-redAmaranth focus:outline-none"
-          type="password"
-          placeholder="Password"
-          value={loginInfo.userPassword}
-          required
-          onChange={(event) =>
-            setLoginInfo((prev) => ({
-              ...prev,
-              userPassword: event.target.value,
-            }))
-          }
-        />
-        <button
-          type="submit"
-          className=" hover:shadow-xl transition duration-300 px-8 py-4 mt-3 text-base font-bold text-center rounded-2xl w-[295px] h-14 font-NotoSans text-whiteSmoke bg-redAmaranth"
-          disabled={isLoading || !loginInfo.userId || !loginInfo.userPassword}
-        >
-          {isLoading ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
-    </div>
+    <>
+      <Link className="mt-20 text-lg font-bold font-Lora " to="/signup">
+        Sign up to continue
+      </Link>
+      <div className="mt-5">
+        <form className="flex flex-col items-center justify-center gap-3" onSubmit={handleSubmit}>
+          <input
+            className="border border-whiteSmoke rounded-2xl w-[295px] h-14 text-center font-NotoSans placeholder:italic focus:border-redAmaranth focus:outline-none"
+            type="text"
+            placeholder="Id"
+            value={loginInfo.userId}
+            autoFocus
+            required
+            onChange={(event) =>
+              setLoginInfo((prev) => ({
+                ...prev,
+                userId: event.target.value,
+              }))
+            }
+          />
+          <input
+            className="border border-whiteSmoke rounded-2xl w-[295px] h-14 text-center font-NotoSans  placeholder:italic focus:border-redAmaranth focus:outline-none"
+            type="password"
+            placeholder="Password"
+            value={loginInfo.userPassword}
+            required
+            onChange={(event) =>
+              setLoginInfo((prev) => ({
+                ...prev,
+                userPassword: event.target.value,
+              }))
+            }
+          />
+          <button
+            type="submit"
+            className=" hover:shadow-xl transition duration-300 px-8 py-4 mt-3 text-base font-bold text-center rounded-2xl w-[295px] h-14 font-NotoSans text-whiteSmoke bg-redAmaranth"
+            disabled={isLoading || !loginInfo.userId || !loginInfo.userPassword}
+          >
+            {isLoading ? 'Logging in...' : 'Login'}
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
