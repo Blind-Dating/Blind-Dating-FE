1de0fa18f02697e558da18256d013a512d53a4db
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-30 14:22:42 +0900

    [Chore] add useGetChatData params type

--- client/src/hooks/api/useGetChat.ts
+++ client/src/hooks/api/useGetChat.ts
@@ -1,14 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { axiosWithAuth } from 'apis/axiosClient';
 
-const fetchChatData = async (roomId: string | undefined, chatId?: number | undefined) => {
+type ChatDataParams = {
+  roomId: string | undefined;
+  chatId?: number | undefined;
+};
+
+const fetchChatData = async (roomId: ChatDataParams, chatId?: ChatDataParams) => {
   const { data } = await axiosWithAuth.get(`api/chatroom/${roomId}`, {
     params: { chatId: chatId || 0 },
   });
   return data;
 };
 
-export const useGetChatData = (roomId: string | undefined, chatId?: number | undefined) => {
+export const useGetChatData = (roomId: ChatDataParams, chatId?: ChatDataParams) => {
   const { isLoading, data, isError, isSuccess } = useQuery(['chatroom'], () =>
     fetchChatData(roomId, chatId)
   );
