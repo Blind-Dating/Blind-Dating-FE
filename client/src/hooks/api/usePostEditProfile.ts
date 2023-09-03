c152a8172bbebe467af50c692e19e45d10ebfe32
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-09-03 22:12:42 +0900

    [Feat] add edit profile custom hook

---
+++ client/src/hooks/api/usePostEditProfile.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { axiosWithAuth } from 'apis/axiosClient';
+import { UserInfo } from 'pages/ProfilePage';
+
+const postEditProfile = async (userInfo: UserInfo) => {
+  const { data } = await axiosWithAuth.put('api/user', userInfo);
+
+  console.log(data);
+  return data;
+};
+
+export const usePostEditProfile = () => {
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(postEditProfile, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['profile']);
+    },
+    onError: (err: Error) => {
+      alert(err.message);
+    },
+  });
+
+  return { mutate };
+};
