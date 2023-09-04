70518cc9b5c4b6315c9399daf3af64306d1b56a4
ted-jv <wkejrdldls@gmail.com>
2023-09-04 19:55:18 +0900

    [Style] modify layout content 부분 css 변경

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ function Layout({ title, children }: { title: string; children: React.ReactNode 
     <div className="flex items-center justify-center h-screen ">
       <div className="container w-[375px] h-[812px] flex flex-col m-auto">
         <Navbar title={title} />
-        <main className="flex-auto">{children}</main>
+        {children}
         <Footer />
       </div>
     </div>
