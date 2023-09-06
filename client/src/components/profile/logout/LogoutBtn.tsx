8dc7fe2012bba93cf4fc9168932394847b4138a3
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-09-07 00:08:03 +0900

    [Style] add logout button markup

---
+++ client/src/components/profile/logout/LogoutBtn.tsx
@@ -0,0 +1,13 @@
+const LogoutBtn = () => {
+  return (
+    <button
+      type="button"
+      className="w-24 h-12 p-3.5 border text-sm text-labelColor border-whiteSmoke rounded-xl hover:border-redAmaranth hover:text-redAmaranth"
+      onClick={() => {}}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default LogoutBtn;
