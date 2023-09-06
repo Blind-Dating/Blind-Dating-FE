f1411ce3b75691d5be3c026323e8c3dfd5cb24f9
ted-jv <wkejrdldls@gmail.com>
2023-09-06 15:42:07 +0900

    [Feat] modify user-atoms data 변경

--- client/src/recoil/user/atoms.ts
+++ client/src/recoil/user/atoms.ts
@@ -1,6 +1,33 @@
 import { atom } from 'recoil';
 
-export const userState = atom<Record<string, string | number>>({
+export type Question = {
+  id: number;
+  status: boolean;
+};
+
+export type UserState = {
+  hasToken: boolean;
+  userId: number;
+  userName: string;
+  region: string;
+  mbti: string;
+  gender: string;
+  interests: string[];
+  questions: Question[];
+  selfIntroduction: string;
+};
+
+export const userState = atom<UserState>({
   key: 'userState',
-  default: { token: '', userName: '', userId: 0 },
+  default: {
+    hasToken: false,
+    userId: 0,
+    userName: '',
+    region: '',
+    mbti: '',
+    gender: '',
+    interests: [],
+    questions: [],
+    selfIntroduction: '',
+  },
 });
