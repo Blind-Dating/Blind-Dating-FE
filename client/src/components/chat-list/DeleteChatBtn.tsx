714ff040e66ad0e91e8bd7062429aa96fb1a74ad
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-09-06 16:52:30 +0900

    [Style] edit chat button style

--- client/src/components/chat-list/DeleteChatBtn.tsx
+++ client/src/components/chat-list/DeleteChatBtn.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as Trash } from 'assets/icons/trash.svg';
 
 const DeleteChatBtn = ({ onClick }: { onClick: () => void }) => {
   return (
-    <button type="button" className="w-8 h-12 pl-2 text-redAmaranth " onClick={onClick}>
+    <button type="button" className="min-w-[28px] h-12 pl-2 text-redAmaranth " onClick={onClick}>
       <Trash />
     </button>
   );
