31c423310fe31164cc89f314abd1c04c7f7334a3
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-09-06 14:07:29 +0900

    [Feat] add chat 전송 버튼

--- client/src/components/chat/ChatForm.tsx
+++ client/src/components/chat/ChatForm.tsx
@@ -12,22 +12,41 @@ const ChatForm = (props: Props) => {
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.code === 'Enter' && !e.shiftKey && !e.nativeEvent.isComposing) {
       e.preventDefault();
+      if (!value.length) {
+        return;
+      }
       const target = e.target as HTMLTextAreaElement;
       onMessage(target.value);
       setValue('');
     }
   };
 
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    onMessage(value);
+    setValue('');
+  };
+
   return (
     <section className="w-full max-h-[15%] px-10 py-8">
-      <textarea
-        className="w-full p-2 text-sm border rounded resize-none border-whiteLilac"
-        placeholder="your message"
-        onKeyDown={handleKeyDown}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        disabled={!roomStatus}
-      />
+      <form className="flex gap-2">
+        <textarea
+          className="w-full p-2 text-sm border rounded resize-none border-whiteLilac focus:outline-redAmaranth/70"
+          placeholder="your message"
+          onKeyDown={handleKeyDown}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!roomStatus}
+        />
+        <button
+          type="button"
+          className="flex-none px-3 text-white rounded text-s disabled:bg-whiteLilac bg-redAmaranth"
+          onClick={handleClick}
+          disabled={!value.length}
+        >
+          전송
+        </button>
+      </form>
     </section>
   );
 };
