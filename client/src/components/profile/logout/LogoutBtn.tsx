f10fc7fcea5c56677f4fff909580f93c519c3e14
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-09-07 16:08:11 +0900

    [Chore] modify usemutation type

--- client/src/components/profile/logout/LogoutBtn.tsx
+++ client/src/components/profile/logout/LogoutBtn.tsx
@@ -2,11 +2,16 @@ import { usePostLogout } from 'hooks/api/usePostLogout';
 
 const LogoutBtn = () => {
   const { mutate } = usePostLogout();
+
+  const handleLogout = () => {
+    mutate();
+  };
+
   return (
     <button
       type="button"
       className="w-24 h-12 p-3.5 border text-sm text-labelColor font-bold border-whiteSmoke rounded-xl hover:border-redAmaranth hover:text-redAmaranth"
-      onClick={() => mutate()}
+      onClick={handleLogout}
     >
       Logout
     </button>
--- client/src/hooks/api/usePostLogout.ts
+++ client/src/hooks/api/usePostLogout.ts
@@ -1,12 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosWithAuth } from 'apis/axiosClient';
+import { AxiosError } from 'axios';
 
-const postLogout = async () => await axiosWithAuth.post('/api/logout');
+type ApiResponse = {
+  status: string;
+  message: string;
+  data: boolean;
+};
+
+const postLogout = async (): Promise<ApiResponse> => await axiosWithAuth.post('/api/logout');
 
 export const usePostLogout = () => {
   const queryClient = useQueryClient();
 
-  const { mutate } = useMutation(postLogout, {
+  const { mutate } = useMutation<ApiResponse, AxiosError>(postLogout, {
     onSuccess: () => {
       queryClient.removeQueries();
     },
