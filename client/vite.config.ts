36e5e84f2366ba4d08ef41bff8cdc2292348c8cc
ted-jv <wkejrdldls@gmail.com>
2023-09-13 13:58:51 +0900

    [Feat] add mkcert https 라이브러리 적용

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  server: {
+    https: true,
+  },
+
+  plugins: [react(), tsconfigPaths(), svgr(), mkcert()],
 });
